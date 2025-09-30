d8d04ce8c3c044d42350793e794decf46784cfd0
DevDominique <162951119+DevDominique@users.noreply.github.com>
2025-09-29 23:16:24 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/SamosaSelector/",
-  build: {
-    rollupOptions: {
-      input: 'index.html',
-    },
-  },
 })
